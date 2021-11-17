9153d96b51529535956dcc8181036f6857261bf4
Hrithik <hrithiksawant80@gmail.com>
2021-11-17 09:14:21 +0530

    sorting

---
+++ DSA-Algorithm-Java/src/com/sorting/cyclicSort.java
@@ -0,0 +1,28 @@
+package com.sorting;
+
+import java.util.Arrays;
+
+public class cyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclic(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            } else {
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr,int first,int secound){
+        int temp = arr[first];
+         arr[first] = arr[secound];
+         arr[secound] = temp;
+    }
+
+}
