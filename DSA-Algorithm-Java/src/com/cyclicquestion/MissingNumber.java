37c93ddf9561fe690a3c3932df1aa29c75b5b896
Hrithik <hrithiksawant80@gmail.com>
2021-11-18 22:42:10 +0530

    question

---
+++ DSA-Algorithm-Java/src/com/cyclicquestion/MissingNumber.java
@@ -0,0 +1,36 @@
+package com.cyclicquestion;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {4,0,2,3,1};
+       int ans = missingNumber(arr);
+        System.out.println(ans);
+    }
+    static int missingNumber(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i];
+            if(arr[i] < arr.length && arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            } else {
+                i++;
+            }
+        }
+        //search for first missing number
+        for(int index = 0; index < arr.length; index++){
+            if(arr[index] != index){
+                return index;
+            }
+        }
+
+        //case 2
+        return arr.length;
+    }
+    static void swap(int[] arr,int first,int secound){
+        int temp = arr[first];
+        arr[first] = arr[secound];
+        arr[secound] = temp;
+    }
+}
