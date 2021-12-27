38bbc004a2f09793dc9208a0d368a5bf09853747
Hrithik <hrithiksawant80@gmail.com>
2021-12-27 08:22:24 +0530

    selection sort

--- DSA-Algorithm-Java/src/com/sorting/selectionSort.java
+++ DSA-Algorithm-Java/src/com/sorting/selectionSort.java
@@ -4,31 +4,54 @@ import java.util.Arrays;
 
 public class selectionSort {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {5,3,2,1,4};
         selection(arr);
         System.out.println(Arrays.toString(arr));
+        selection1(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
-    static void selection(int[] arr){
-        for(int i = 0; i < arr.length; i++){
+
+    static void selection (int[] arr){
+        for(int i = 0; i < arr.length;i++){
             //find the max item in the remaining array and swap with its correct index
-            int last = arr.length - i - 1;
+
+            int last = arr.length - 1 - i;
             int maxIndex = getMaxIndex(arr,0,last);
             swap(arr,maxIndex,last);
-
         }
     }
 
-    static void swap(int[] arr,int first,int secound){
-        int temp = arr[first];
-        arr[first] = arr[secound];
-        arr[secound] = temp;
+    //this is for min logic
+    static void selection1 (int[] arr){
+        for(int i = arr.length-1; i >= 0;i--){
+
+            int first = arr.length - 1 - i ;
+            int minIndex = getMinIndex(arr,first);
+            swap(arr,minIndex,first);
+        }
     }
 
-    static int getMaxIndex(int[] arr, int start, int end) {
-        int max = start;
+    static int getMinIndex(int[] arr, int first) {
+        int min= first;
+        for(int i = first;i< arr.length;i++){
+            if(arr[min]>arr[i]){
+                min = i;
+            }
+        }
+        return min;
+    }
 
-        for(int i = start; i <= end; i++){
-            if(arr[max] < arr[i]){
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int  getMaxIndex(int[] arr,int start,int end){
+        int max = start;
+        for(int i=  start;i <= end; i++){
+            if(arr[max]<arr[i]){
                 max = i;
             }
         }
